7a451a9551fc60450215317265ff0eaa3ee3827d
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-05-26 23:42:23 +0330

    indx js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import StarRating from './StarRating';
+import './index.css';
+import App from './AppMovieRating';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
